f1b86dc4135544845de4fe1964373f746859ffa7
1337_DaKL <luongtrinh2k3ndad@gmail.com>
2025-05-14 16:52:28 +0700

    update read file

--- WinAPI_Gui_Project/src/main.cpp
+++ WinAPI_Gui_Project/src/main.cpp
@@ -2,12 +2,11 @@
 #include <string>
 #include <iostream>
 using namespace std;
-int main()
-{
+void repeat_all_functions(){
     HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
     HANDLE hIn = GetStdHandle(STD_INPUT_HANDLE);
     if (hOut == INVALID_HANDLE_VALUE || hIn == INVALID_HANDLE_VALUE)
-        return 1;
+        return ;
     const char *msg = "Hay chon chuc nang ban can\n0. Thoat chuong trinh\n1. Doc file in noi dung len man hinh console\n2. Ghi mot chuoi vao file\n3. Xoa file\nNhap chuc nang ban mong muon:\n";
     DWORD written = 0;
     WriteConsole(hOut, msg, (DWORD)strlen(msg), &written, nullptr);
@@ -31,7 +30,7 @@ int main()
                 const char *msg_error_read_file = "Khong the doc duoc file hoac khong tim thay file!!";
                 DWORD written = 0;
                 WriteConsole(hOut, msg_error_read_file, (DWORD)strlen(msg_error_read_file), &written, nullptr);
-                return 1;
+                return ;
             }
             const DWORD buffer_size = 1000;
             char buffer[buffer_size];
@@ -47,13 +46,28 @@ int main()
                 const char *msg_error_read_file = "Xay ra loi khi doc file";
                 DWORD written = 0;
                 WriteConsole(hOut, msg_error_read_file, (DWORD)strlen(msg_error_read_file), &written, nullptr);
-                return 1;
+                return ;
             }
             buffer[bytes_read] = '\0';
-            std::string msg_result_read_file = std::string("Ket qua doc tu file la: \n") + buffer + "\n";
+            std::string msg_result_read_file = std::string("Ket qua doc tu file la: \n\"") + buffer + "\"\nNhap 0 de quay lai hoac 1 de ket thuc chuong trinh\n";
             DWORD written = 0;
             WriteConsole(hOut, msg_result_read_file.c_str(), (DWORD)msg_result_read_file.length(), &written, nullptr);
-            return 1;
+            wchar_t choose_t[100];    
+            DWORD read_console_t;
+            ReadConsoleW(hIn, choose_t, 100, &read_console_t, nullptr);
+            int result_choose_t = *choose_t - 48;
+            if(result_choose_t == 0){
+                CloseHandle(hFile);
+                repeat_all_functions();
+            }
+            else if(result_choose_t == 1){
+                return ;
+            }
+            else{
+                const char *msg_error_read_file = "Vui long chon dung lua chon co trong cac lua chon tren";
+                DWORD written = 0;
+                WriteConsole(hOut, msg_error_read_file, (DWORD)strlen(msg_error_read_file), &written, nullptr);
+            }
         }
         else if (result_choose == 2){
             cout << "chon 2\n";
@@ -70,12 +84,9 @@ int main()
             cout << "Vui long chon dung so chuc nang ban can chon\n";
         }
     }
-    return 0;
+    return ;
 }
-// Gui
-//  int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-//                  PSTR szCmdLine, int iCmdShow)
-//  {
-//      MessageBox(NULL, TEXT("Hello, Windows!"), TEXT("HelloMsg"), MB_OK);
-//      return 0;
-//  }
+int main()
+{
+    repeat_all_functions();
+}
